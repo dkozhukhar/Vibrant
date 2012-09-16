b8ecfa395748a68f7e03b95b5fa438ba85eec622
ponce <aliloko@gmail.com>
2012-09-16 14:28:08 +0200

    hogenous template allocate :(

--- vibrantprogram.d
+++ vibrantprogram.d
@@ -73,9 +73,8 @@ class VibrantProgram : SDLApp
             m_view = box2i(0, 0, width, height).subRectWithRatio(ratio);
             GL.check();
             info(">OpenGL settings");
-            GL.disable(GL.DEPTH_TEST, GL.LINE_SMOOTH, GL.POLYGON_SMOOTH, GL.POINT_SMOOTH, GL.BLEND,
-                      GL.FOG, GL.LIGHTING, GL.NORMALIZE, GL.STENCIL_TEST, GL.CULL_FACE);
-        
+            GL.disable(GL.DEPTH_TEST, GL.LINE_SMOOTH, GL.POLYGON_SMOOTH, GL.POINT_SMOOTH);
+            GL.disable(GL.BLEND, GL.FOG, GL.LIGHTING, GL.NORMALIZE, GL.STENCIL_TEST, GL.CULL_FACE);
             GL.disable(GL.AUTO_NORMAL, GL.DITHER, GL.FOG, GL.LIGHTING, GL.NORMALIZE);        
             GL.disable(GL.POLYGON_SMOOTH, GL.LINE_SMOOTH, GL.POINT_SMOOTH);
 
